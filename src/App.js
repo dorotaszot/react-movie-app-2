f77de9fee1e2008c34faee85f04e1c56536f3796
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-02-17 20:13:28 +0100

    almost done, remove from favourites not finished yet

--- src/App.js
+++ src/App.js
@@ -4,31 +4,49 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddToFavourites from './components/AddFavourites';
+import RemoveFromFavourites from './components/RemoveFromFavourites';
+
+
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
+  const [favourites, setFavourites] = useState([]);
 
   const getMoviesRequest = async () => {
-    const response = await fetch('http://www.omdbapi.com/?s=when&apikey=2821e511');
+    const response = await fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=2821e511`);
     const data = await response.json();
-    setMovies(data.Search);
+    if(data.Search) {
+      setMovies(data.Search);
+    }
+    
     // console.log(data.Search);
   }
 
   useEffect(() => {
-    getMoviesRequest();
-  }, [])
+    getMoviesRequest(searchValue);
+  }, [searchValue])
 
+  const addMoviesToFavourites = (movie) => {
+    const newFavouritesArr = [...favourites, movie];
+    setFavourites(newFavouritesArr);
+  }
 
   return (
     <div className="container-fluid movie-app">
-      <div className="row d-flex align-items-center justify-content-between">
+      <div className="row d-flex align-items-center   justify-content-between">
         <MovieListHeading heading='Movies' />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
       <div className="row">
-        <MovieList movies={movies} /> 
+        <MovieList movies={movies} handleFavouritesClick={addMoviesToFavourites} moveFavourites={AddToFavourites}/> 
+      </div>
+      <div className="row d-flex align-items-center justify-content-between">
+        <MovieListHeading heading='Favourites' />
+        </div>
+        <div className="row">
+          <MovieList movies={favourites} handleFavouritesClick={addMoviesToFavourites} moveFavourites={RemoveFromFavourites} /> 
       </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import AddFavourites from './AddFavourites';
+
 
 const MovieList = (props) => {
+  const MoveFavouritesComponent = props.moveFavourites;
   return (
     <>
       {props.movies.map( (movie) => (
-        <div className="m-3">
-          <img src={movie.Poster} alt="poster"/>
+        <div className="m-3 images-container d-flex ">
+          <img src={movie.Poster} alt="poster" className=""/>
+          <div className="overlay p-3 text-center" onClick={() => props.handleFavouritesClick(movie)}>
+            <MoveFavouritesComponent />
+          </div>
         </div>
       ))}
     </>
